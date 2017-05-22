1b5cd2d01a42bf1d9aeeb902e2a0da207d29119a
masteraaa <masteraaa@imac2.local>
2017-05-22 19:03:29 +0200

    listado ingredientes

--- recetas/VerRecetaViewCellViewController.swift
+++ recetas/VerRecetaViewCellViewController.swift
@@ -8,11 +8,11 @@
 
 import UIKit
 
-class VerRecetaViewCellViewController: UIViewController {
+class VerRecetaViewCellViewController: UIViewController,UITableViewDataSource {
     
     @IBOutlet weak var titleRecetaView: UINavigationItem!
     @IBOutlet weak var imagenRecetaView: UIImageView!
-    @IBOutlet weak var nombreRectaView: UILabel!
+    @IBOutlet weak var tabla: UITableView!
     
     var receta:Receta?
 
@@ -33,15 +33,6 @@ class VerRecetaViewCellViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    numerOfRose
+    
 }
